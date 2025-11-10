4b776ff8fb60c59c8febe1e976f94e8348949e33
quanphammm8-ops <quanphammm8@gmail.com>
2025-11-10 23:53:25 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import Page3 from "./pages/Page3";
@@ -11,6 +11,7 @@ import {
   Routes,
   useLocation,
 } from "react-router-dom";
+import music from "./assets/music.mp3";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -22,10 +23,39 @@ function ScrollToTop() {
   return null;
 }
 
+function BackgroundMusic() {
+  const [audio] = useState(() => new Audio(music));
+
+  useEffect(() => {
+    const tryPlay = () => {
+      audio.loop = true;
+      audio.play().catch(() => {
+        console.log("Autoplay block, interact waiting...");
+      });
+    };
+
+    tryPlay();
+
+    const handleUserInteraction = () => {
+      audio.play().catch(() => {});
+      window.removeEventListener("click", handleUserInteraction);
+    };
+    window.addEventListener("click", handleUserInteraction);
+
+    return () => {
+      audio.pause();
+      window.removeEventListener("click", handleUserInteraction);
+    };
+  }, [audio]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
+      <BackgroundMusic />
       <Routes>
         <Route path="/" element={<Navigate to={"/home"} replace />} />
         <Route path={"/home"} element={<Page1 />} />
